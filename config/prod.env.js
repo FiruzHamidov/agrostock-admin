19d13ff4611efabcdd9f9237e345efa3ce5f1c8a
Ali Mamaev <mamaev-ali@inbox.ru>
2021-05-03 14:01:46 +0300

    fix api

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://api.agrostock.org"',
-  FILE_URL: '"https://api.agrostock.org"',
+  BASE_API: '"https://api.agrostock.pro"',
+  FILE_URL: '"https://api.agrostock.pro"',
   SOCKET_PATH: '"/socket.io"',
 }
